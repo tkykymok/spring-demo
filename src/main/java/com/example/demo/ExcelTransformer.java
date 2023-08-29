e2fa96c11efe39b0243106ffc57318554bd1d3f0
tkykymok <tkykymok2@icloud.com>
2023-08-30 08:30:03 +0900

    全体的にリファクタリング

--- src/main/java/com/example/demo/ExcelTransformer.java
+++ src/main/java/com/example/demo/ExcelTransformer.java
@@ -19,42 +19,22 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelTransformer {
+    // 定数の定義
+    private static final String SAMPLE_DATA_PATH = "サンプルデータ.xlsx";
+    private static final String TEMP_FILE_PATH = "temp.xlsx";
+    private static final String TEMPLATE_SHEET_NAME = "template";
 
     public static void main(String[] args) throws Exception {
         // Excelファイルを読み込み
-        File sampleDataFile = new File("サンプルデータ.xlsx");
-        Workbook sampleDataWb = new XSSFWorkbook(sampleDataFile);
-        Sheet sourceSheet = sampleDataWb.getSheet("Sheet1");
-        sampleDataWb.close();
-
-        // シートからPickingInfoのリストを抽出
-        List<PickingInfo> pickingInfoList = extractDataFromSheet(sourceSheet);
+        List<PickingInfo> pickingInfoList = loadPickingInfoFromExcel(SAMPLE_DATA_PATH);
         // PickingInfoのリストを変換してデータマップを作成
         Map<PickingInfoKey, Map<DeliveryField, Map<String, Integer>>> dataMap = transformData(pickingInfoList);
-
         System.out.println(dataMap);
 
-        // 一時ファイルのパス
-        Path tempPath = Paths.get("temp.xlsx");
-        // ファイルをコピー（既に存在する場合は上書き）
-        Files.copy(Paths.get("サンプル.xlsx"), tempPath, StandardCopyOption.REPLACE_EXISTING);
-
-        // テンプレートシートを取得
-        File file = new File("temp.xlsx");
-        Workbook workbook = new XSSFWorkbook(file);
-        Sheet templateSheet = workbook.getSheet("template");
-
-        // 出力用のシートを作成
-        Sheet outputSheet = createSheet(workbook, "ピッキングシート");
-
-        // 出力シートにデータを埋め込む
-        fillOutputSheet(dataMap, templateSheet, outputSheet);
-        // ワークブックを新しいExcelファイルとして保存
-        LocalDateTime time = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        String formattedTime = time.format(formatter);
-        saveWorkbookToFile(workbook, formattedTime + "_ピッキングシート_サンプル.xlsx");
-
+        // テンプレートを一時ファイルにコピー
+        Path tempPath = copyTemplateToTempFile();
+        // データを新しいExcelファイルに埋め込み
+        embedDataToNewExcelFile(dataMap, tempPath);
         // 一時的に作成したファイルを削除
         Files.delete(tempPath);
     }
@@ -71,53 +51,70 @@ public class ExcelTransformer {
 
         int outputRowNum = 1;
         for (Map.Entry<PickingInfoKey, Map<DeliveryField, Map<String, Integer>>> entry : dataMap.entrySet()) {
-            // キーを取得
             PickingInfoKey info = entry.getKey();
-            // 項目、店舗毎の値、
             Map<DeliveryField, Map<String, Integer>> perItemMap = entry.getValue();
-            // 店舗コード一覧を取得
+
             Collection<Map<String, Integer>> collection = perItemMap.values();
             Map<String, Integer> firstMap = collection.iterator().next();
             List<String> storeCdList = new ArrayList<>(firstMap.keySet());
 
-            // 初回ループ時はヘッダー部分を設定、それ以外はヘッダー部分の設定をスキップ
-            for (int j = outputRowNum == 1 ? 1 : 3; j <= templateSheet.getLastRowNum(); j++) {
+            // 初回のループのみヘッダー項目を設定(テンプレート1行目から)、それ以外はスキップ(テンプレート3行目からß)
+            int startRow = outputRowNum == 1 ? 1 : 3;
+            // 行のループ
+            for (int j = startRow; j <= templateSheet.getLastRowNum(); j++) {
                 Row templateRow = templateSheet.getRow(j);
                 Row newRow = outputSheet.createRow(outputRowNum++);
 
+                // 対象行、列のループ
                 for (int k = 1; k < templateRow.getLastCellNum(); k++) {
-                    Cell templateCell = templateRow.getCell(k);
-                    Cell newCell = newRow.createCell(k);
-                    newCell.setCellStyle(templateCell.getCellStyle());
-                    newCell.setCellValue(templateCell.getStringCellValue());
-
-                    DeliveryField fieldName = DeliveryField.fromString(templateCell.getStringCellValue());
-                    if (fieldName == null) {
-                        String fieldValue = info.getFieldValueByName(templateCell.getStringCellValue());
-                        if (fieldValue != null) {
-                            newCell.setCellValue(fieldValue);
-                        }
-                    } else {
-                        switch (fieldName) {
-                            case 店舗:
-                                fillCells(newRow, k, storeCdList, templateCell, storeCd -> storeCd);
-                                break;
-                            case 店舗在庫:
-                            case 店舗積送:
-                            case 配分数:
-                            case 基準在庫:
-                                fillCells(newRow, k, storeCdList, templateCell, storeCd -> perItemMap.get(fieldName).get(storeCd));
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-
+                    setCellContentAndStyle(newRow, k, info, storeCdList, perItemMap, templateRow);
                     outputSheet.setColumnWidth(k, templateSheet.getColumnWidth(k));
                 }
             }
         }
     }
+
+    /**
+     * セルに値とスタイルを設定します
+     *
+     * @param newRow
+     * @param k
+     * @param info
+     * @param storeCdList
+     * @param perItemMap
+     * @param templateRow
+     */
+    private static void setCellContentAndStyle(Row newRow, int k, PickingInfoKey info, List<String> storeCdList,
+                                               Map<DeliveryField, Map<String, Integer>> perItemMap,
+                                               Row templateRow) {
+        Cell templateCell = templateRow.getCell(k);
+        Cell newCell = newRow.createCell(k);
+        newCell.setCellStyle(templateCell.getCellStyle());
+        newCell.setCellValue(templateCell.getStringCellValue());
+
+        DeliveryField fieldName = DeliveryField.fromString(templateCell.getStringCellValue());
+        if (fieldName == null) {
+            String fieldValue = info.getFieldValueByName(templateCell.getStringCellValue());
+            if (fieldValue != null) {
+                newCell.setCellValue(fieldValue);
+            }
+        } else {
+            switch (fieldName) {
+                case 店舗:
+                    fillCells(newRow, k, storeCdList, templateCell, storeCd -> storeCd);
+                    break;
+                case 店舗在庫:
+                case 店舗積送:
+                case 配分数:
+                case 基準在庫:
+                    fillCells(newRow, k, storeCdList, templateCell, storeCd -> perItemMap.get(fieldName).get(storeCd));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
 
     /**
      * 値を持つセルにデータを埋め込むメソッド
@@ -199,7 +196,6 @@ public class ExcelTransformer {
             removeSheet(workbook, "template");
             workbook.write(outputStream);
         }
-        workbook.close();
     }
 
     // 既存のCellStyleをコピーするヘルパーメソッド
@@ -207,6 +203,35 @@ public class ExcelTransformer {
         CellStyle newStyle = workbook.createCellStyle();
         newStyle.cloneStyleFrom(oldStyle);
         return newStyle;
+    }
+
+    private static List<PickingInfo> loadPickingInfoFromExcel(String path) throws Exception {
+        try (Workbook sampleDataWb = new XSSFWorkbook(new File(path))) {
+            Sheet sourceSheet = sampleDataWb.getSheet("Sheet1");
+            return extractDataFromSheet(sourceSheet);
+        }
+    }
+
+    private static Path copyTemplateToTempFile() throws Exception {
+        Path tempPath = Paths.get(ExcelTransformer.TEMP_FILE_PATH);
+        // ファイルをコピー（既に存在する場合は上書き）
+        Files.copy(Paths.get(SAMPLE_DATA_PATH), tempPath, StandardCopyOption.REPLACE_EXISTING);
+        return tempPath;
+    }
+
+    private static void embedDataToNewExcelFile(Map<PickingInfoKey, Map<DeliveryField, Map<String, Integer>>> dataMap, Path tempPath) throws Exception {
+        try (Workbook workbook = new XSSFWorkbook(tempPath.toFile())) {
+            Sheet templateSheet = workbook.getSheet(TEMPLATE_SHEET_NAME);
+            // 出力用のシートを作成
+            Sheet outputSheet = createSheet(workbook, "ピッキングシート");
+            // 出力シートにデータを埋め込む
+            fillOutputSheet(dataMap, templateSheet, outputSheet);
+            // ワークブックを新しいExcelファイルとして保存
+            LocalDateTime time = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+            String formattedTime = time.format(formatter);
+            saveWorkbookToFile(workbook, formattedTime + "_ピッキングシート_サンプル.xlsx");
+        }
     }
 
     /**
